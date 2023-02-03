8f725df88b7c663540a118800481e25fc6c5a78c
elandres1 <andres.angulob@pca.edu.co>
2023-02-03 00:26:23 -0500

    updae

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
-import { UserController } from '../controllers/user.controller';
+import { UserController } from '../users/user.controller';
 import { RandomController } from '../controllers/random.controller';
 import { AppService } from '../services/app.service';
-import { UserService } from '../services/user.service';
+import { UserService } from '../users/user.service';
 import { RandomService } from '../services/random.service';
 
 @Module({
